1c7563271b4fdee21d992076c725d3c656f27e1c
Moshekwa <malatjim04@gmail.com>
2025-03-04 21:44:30 +0200

    dynamic validation of amount

--- backend/schemas/transaction_schema.py
+++ backend/schemas/transaction_schema.py
@@ -4,14 +4,14 @@ from typing import Literal
 
 # Input Schema for Transaction Creation and Update
 class TransactionInput(BaseModel):
-    amount: conint(ge=-1_000_000, le=1_000_000) # We assume the range to be between these two values and allow for -ve
+    amount: float # We assume the range to be between these two values and allow for -ve
     type: Literal["credit", "debit"]  # Type must be either "credit" or "debit"
     description: constr(min_length=1, max_length=255)  # Description must be a non-empty string
 
 # Output Schema for Transaction Response
 class TransactionOutput(BaseModel):
     id: int
-    amount: int
+    amount: float
     date: datetime # YYYY-MM-DD
     type: Literal["credit", "debit"]
     description: str
